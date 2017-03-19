e5af7dcf0b0550f23fd79cd1e149264b351e585a
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-03-19 01:25:03 -0400

    migrated game routes to MVC

--- server/routes/games.js
+++ server/routes/games.js
@@ -3,122 +3,35 @@ let express = require('express');
 // create the router for our application
 let router = express.Router();
 
-// import mongoose NPM package
-let mongoose = require('mongoose');
-let passport = require('passport');
-
-// define the user model
-let UserModel = require('../models/users');
-let User = UserModel.User; // alias for User
-
 // create the game object - represents a document in the
 // games collection
 let game = require('../models/games');
 
-// function to check if the user is authenticated
-function requireAuth(req, res, next) {
-  // check if the user is logged index
-  if(!req.isAuthenticated()) {
-    return res.redirect('/login');
-  }
-  next();
-}
+// require the users controller for authentication
+let usersController = require('../controllers/users');
 
-/* GET games List page. READ */
-router.get('/', requireAuth, (req, res, next) => {
-  // find all games in the games collection
-  game.find((err, games) => {
-    if (err) {
-      return console.error(err);
-    } else {
-      res.render('games/index', {
-        title: 'Games',
-        games: games,
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-  });
-});
+// require the games controller to access games collection in findById
+let gamesController = require('../controllers/games');
 
 // GET the Game Details page in order to add a new Game
-router.get('/add', requireAuth, (req, res, next) => {
-    res.render('games/details', {
-      title: 'Add a new Game',
-      games: '',
-      displayName: req.user ? req.user.displayName : ''
-    });
+router.get('/add', usersController.RequireAuth, (req, res, next) => {
+  gamesController.DisplayAdd(req, res);
+}).post('/add', usersController.RequireAuth, (req, res, next) => {
+  // POST process the Game Details page and create a new Game - CREATE
+  gamesController.CreateGame(req, res);
 });
 
-// POST process the Game Details page and create a new Game - CREATE
-router.post('/add', requireAuth, (req, res, next) => {
-    game.create({
-      "name": req.body.name,
-      "cost": req.body.cost,
-      "rating": req.body.rating
-    }, (err, game) => {
-      if(err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        res.redirect('/games');
-      }
-    });
-});
-
- // GET the Game Details page in order to edit a new Game
-router.get('/:id', requireAuth, (req, res, next) => {
-  // get a reference to the id of the game to edit
-  let id = req.params.id;
-  // find the game to edit by it's id in the games collection
-  game.findById(id, (err, games) => {
-    if (err) {
-      console.error(err);
-      res.end(error);
-    } else {
-      // show the edit view
-      res.render('games/details', {
-        title: 'Game Details',
-        games: games,
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-  });
-});
-
-// POST - process the information passed from the details form and update the document
-router.post('/:id', requireAuth, (req, res, next) => {
-  // get a reference to the id of the game to edit
-  let id = req.params.id;
-  // create a new games object to hold the changes
-  let games = new game({
-    "_id": id,
-    "name": req.body.name,
-    "cost": req.body.cost,
-    "rating": req.body.rating
-  });
-  game.update({ _id: id}, games, (err) => {
-    if(err) {
-      console.log(err);
-      res.end(error);
-    } else {
-      // refresh the games list
-      res.redirect('/games');
-    }
-  });
+// GET the Game Details page in order to edit a new Game
+router.get('/:id', usersController.RequireAuth, (req, res, next) => {
+  gamesController.DisplayEdit(req, res);
+}).post('/:id', usersController.RequireAuth, (req, res, next) => {
+  // POST - process the information passed from the details form and update the document
+  gamesController.UpdateGame(req, res);
 });
 
  // GET - process the delete by user id
-router.get('/delete/:id', requireAuth, (req, res, next) => {
-  // get a reference to the id of the game to edit
-  let id = req.params.id;
-  game.remove({_id: id}, (err) => {
-    if(err) {
-      console.log(err);
-      res.end(err);
-    } else {
-      res.redirect('/games');
-    }
-  });
+router.get('/delete/:id', usersController.RequireAuth, (req, res, next) => {
+  gamesController.DeleteGame(req, res);
 });
 
 module.exports = router;
