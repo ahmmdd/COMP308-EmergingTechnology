5781bfae586211f8a768a73618ea8eab847fc044
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-03-18 22:16:03 -0400

    added content templates for each page

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,35 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  let currentDate = new Date();
-  res.render('index', {
+  res.render('content/index', {
     title: 'Home',
-    date: currentDate.getHours()
  });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'About' });
+  res.render('content/about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', {
+    title: 'Projects'
+   });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', {
+    title: 'Services'
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact'
+   });
 });
 
 module.exports = router;
