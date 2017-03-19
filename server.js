a66dce4ee406c70194f05a9c80ef95556c14da03
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-03-18 21:29:07 -0400

    migrated to express-generated site

--- server.js
+++ server.js
@@ -1,37 +1,96 @@
+#!/usr/bin/env node
 /**
  * Name: Mohammed Juned Ahmed
  * Student ID: 300833356
  * File: server.js
- * Description: Express server.
+ * Description: Module dependencies.
  */
 
-// imported third party module 'express'
-let express = require('express');
-let app = express();
+/**
+ * Module dependencies.
+ */
 
-// assigned a port to constant
-const localport = 3000;
+var app = require('./app');
+var debug = require('debug')('comp308-w2017-lesson3:server');
+var http = require('http');
 
 /**
  * Get port from environment and store in Express.
  */
-let port = process.env.PORT || localport;
+
+var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// setup a event loop - create port 3000
-app.listen(port);
-console.log(`Server started at http://localhost:${port}`);
+/**
+ * Create HTTP server.
+ */
 
-/* ROUTING - mounted routes */
+var server = http.createServer(app);
 
-// second route is '/hello'
-app.use('/hello', (req, res, next) =>{
-    res.send('Hello World!');
-});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// first route is '/' root of my website
-app.use('/', (req, res, next) =>{
-    res.send('Welcome!');
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-module.exports = app;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
